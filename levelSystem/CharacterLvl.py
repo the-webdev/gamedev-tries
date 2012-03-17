c0dbc99c690af78a78927957a901558424f4e19f
Jan S <sowhatisthequestion@gmail.com>
2012-03-17 22:53:58 +0100

    Code-Cleanup

--- levelSystem/CharacterLvl.py
+++ levelSystem/CharacterLvl.py
@@ -1,4 +1,10 @@
-import math
+import math, const
+START_AD = 10
+MAX_LEVELS = 100
+START_MRESISTANCE = 3
+START_HP = 50
+START_AP = 7
+START_BRESISTANCE = 1
 class CharacterLvl:
       def calculate(self, atLvl):
           def calc_addit(lvlNum, addit):
@@ -8,3 +14,7 @@ class CharacterLvl:
                     'AP':START_AD+calc_addit(atLvl,0.7),
                     'MRES':START_MRESISTANCE+calc_addit(atLvl,0.7),
                     'BRES':START_BRESISTANCE+calc_addit(atLvl,0.7)}
+if __name__ == "__main__":
+	cl = CharacterLvl()
+	print "Stats bei LVL 12:"
+	print cl.calculate(12)	
