a60ce6ccea8b822b00d53cace7a9d259d818ea53
J. Stratmann <sowhatisthequestion@gmail.com>
2012-03-19 20:38:20 +0100

    Detailaenderungen, neuen Branch extra fuer Tests hinzugefuegt

--- levelSystem/CharacterLvl.py
+++ levelSystem/CharacterLvl.py
@@ -6,7 +6,7 @@ class CharacterLvl:
     if type(ch) == type(CharacterValues()):
             if(ch.dict_additiveValues != {}) and (ch.dict_startValues != {}):
                 self.additValues = ch.dict_additiveValues
-                self.startValues = ch.dict_startValues  
+                self.startValues = ch.dict_startValues
   def calculate(self, atLvl):
     def calc_addit(lvlNum, addit):
           return lvlNum*addit
@@ -15,9 +15,5 @@ class CharacterLvl:
         #       'AD':self.startValues['AD']+calc_addit(atLvl,self.additValues['AD']),
         #      'AP':self.startValues['AP']+calc_addit(atLvl,self.additValues['AP']),
         #     'MRES':self.startValues['MRES']+calc_addit(atLvl,self.additValues['MRES'],
-        #    'ARMOR':self.startValues['ARMOR']+calc_addit(atLvl,self.additValues['ARMOR'])}             
-      return dict(HEALTH=self.startValues['HEALTH']+calc_addit(atLvl,self.additValues['HEALTH']),
-                  AD=self.startValues['AD']+calc_addit(atLvl,self.additValues['AD']),
-                  AP=self.startValues['AP']+calc_addit(atLvl,self.additValues['AP']),
-                  MRES=self.startValues['MRES']+calc_addit(atLvl,self.additValues['MRES']),
-                  ARMOR=self.startValues['ARMOR']+calc_addit(atLvl,self.additValues['ARMOR'])
+        #    'ARMOR':self.startValues['ARMOR']+calc_addit(atLvl,self.additValues['ARMOR'])}
+        return dict(HEALTH=self.startValues['HEALTH']+calc_addit(atLvl,self.additValues['HEALTH']),AD=self.startValues['AD']+calc_addit(atLvl,self.additValues['AD']),AP=self.startValues['AP']+calc_addit(atLvl,self.additValues['AP']),MRES=self.startValues['MRES']+calc_addit(atLvl,self.additValues['MRES']),ARMOR=self.startValues['ARMOR']+calc_addit(atLvl,self.additValues['ARMOR'])        
